603cf39ac3a16adf2a4c8fecd877a43e5fd4906e
xuesemofa@126.com <xuesemofa@126.com>
2018-06-22 13:33:13 +0800

    用jwt登录

--- src/main/java/org/ld/bt2/business/login/controller/LoginController.java
+++ src/main/java/org/ld/bt2/business/login/controller/LoginController.java
@@ -3,7 +3,6 @@ package org.ld.bt2.business.login.controller;
 import org.ld.bt2.business.account.model.AccountModel;
 import org.ld.bt2.business.account.service.AccountService;
 import org.ld.bt2.config.shiro.jwt.JWTUtil;
-import org.ld.bt2.config.shiro.token.JWTToken;
 import org.ld.bt2.util.resultJson.ResponseResult;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,10 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import static org.apache.shiro.SecurityUtils.getSubject;
 
 /**
  * @author ld
@@ -31,15 +27,15 @@ public class LoginController {
 
     @RequestMapping(value = "/login", method = RequestMethod.POST)
     public ResponseResult<String> login(@RequestBody AccountModel model,
-                                        HttpServletRequest request,
                                         HttpServletResponse response) {
         ResponseResult<AccountModel> result = service.findByAccount(model.getAccount());
         if (result.isSuccess()) {
-            String sign = JWTUtil.sign(result.getData().getAccount(), result.getData().getPassword());
-            JWTToken token = new JWTToken(sign);
-            getSubject().login(token);
-//            response.setHeader("Authorization", sign);
-            return new ResponseResult<>(true, "登录成功", null);
+            if (model.getAccount().equals(model.getPassword())) {
+                String sign = JWTUtil.sign(result.getData().getAccount(), result.getData().getPassword());
+                response.setHeader("Authorization", sign);
+                return new ResponseResult<>(true, "登录成功", null);
+            } else
+                return new ResponseResult<>(false, "账号密码错误", null);
         } else
             return new ResponseResult<>(false, result.getMessage(), null);
     }
