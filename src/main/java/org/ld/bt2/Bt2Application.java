19d929264489c860c16cc2e12e55e75008ba52cf
xuesemofa@126.com <xuesemofa@126.com>
2018-06-22 12:03:05 +0800

    用jwt登录

--- src/main/java/org/ld/bt2/Bt2Application.java
+++ src/main/java/org/ld/bt2/Bt2Application.java
@@ -3,6 +3,7 @@ package org.ld.bt2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
 
 //springboot
 @SpringBootApplication
@@ -13,5 +14,4 @@ public class Bt2Application {
     public static void main(String[] args) {
         SpringApplication.run(Bt2Application.class, args);
     }
-
 }
--- src/main/java/org/ld/bt2/business/login/controller/LoginController.java
+++ src/main/java/org/ld/bt2/business/login/controller/LoginController.java
@@ -3,6 +3,7 @@ package org.ld.bt2.business.login.controller;
 import org.ld.bt2.business.account.model.AccountModel;
 import org.ld.bt2.business.account.service.AccountService;
 import org.ld.bt2.config.shiro.jwt.JWTUtil;
+import org.ld.bt2.config.shiro.token.JWTToken;
 import org.ld.bt2.util.resultJson.ResponseResult;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,7 +11,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import static org.apache.shiro.SecurityUtils.getSubject;
 
 /**
  * @author ld
@@ -27,11 +31,14 @@ public class LoginController {
 
     @RequestMapping(value = "/login", method = RequestMethod.POST)
     public ResponseResult<String> login(@RequestBody AccountModel model,
+                                        HttpServletRequest request,
                                         HttpServletResponse response) {
         ResponseResult<AccountModel> result = service.findByAccount(model.getAccount());
         if (result.isSuccess()) {
-            response.setHeader("Authorization",
-                    JWTUtil.sign(result.getData().getAccount(), result.getData().getPassword()));
+            String sign = JWTUtil.sign(result.getData().getAccount(), result.getData().getPassword());
+            JWTToken token = new JWTToken(sign);
+            getSubject().login(token);
+//            response.setHeader("Authorization", sign);
             return new ResponseResult<>(true, "登录成功", null);
         } else
             return new ResponseResult<>(false, result.getMessage(), null);
