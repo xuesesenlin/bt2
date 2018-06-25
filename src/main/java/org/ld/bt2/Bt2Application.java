07b36635248de8d1c28e28cfdb711561b4aedc36
xuesemofa@126.com <xuesemofa@126.com>
2018-06-25 13:52:29 +0800

    用jwt登录

--- src/main/java/org/ld/bt2/Bt2Application.java
+++ src/main/java/org/ld/bt2/Bt2Application.java
@@ -3,7 +3,7 @@ package org.ld.bt2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 //springboot
 @SpringBootApplication
